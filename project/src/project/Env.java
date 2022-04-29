fe7f58983a472df9ab9e295e0428ab959a8f5344
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2022-04-29 20:36:58 +0300

    Added circumNavigate() function

--- project/src/project/Env.java
+++ project/src/project/Env.java
@@ -6,9 +6,13 @@ import simbad.sim.*;
 
 public class Env extends EnvironmentDescription {
     public Env(Point3d light, Point3d goal) {
-        add(new MyRobot(new Vector3d(0, 0, 0), "my robot", goal));
+        add(new MyRobot(new Vector3d(0, 0, 0), "my robot"));
         add(new CherryAgent(new Vector3d(goal), "goal", 0.1f));
         light1SetPosition(light.x, light.y, light.z);
         light2SetPosition(light.x, light.y, light.z);
+        
+        Wall wall1 = new Wall(new Vector3d(-3, 0, 0), 10, 1, this);
+        wall1.rotate90(1);
+        add(wall1);
     }
 }
--- project/src/project/MyRobot.java
+++ project/src/project/MyRobot.java
@@ -12,13 +12,13 @@ enum RobotStatus {
 public class MyRobot extends Agent {
     LightSensor leftLight;
     LightSensor rightLight;
+    RangeSensorBelt sonars;
     RobotStatus status;
     double leftLux;
     double rightLux;
     double prevLeftLux;
     double prevRightLux;
     int cnt;
-    Point3d goal;
     
     static double K_ROT = 10;
     static double K_ROT_2 = 0.03;
@@ -26,21 +26,24 @@ public class MyRobot extends Agent {
     static double K_TRANSL_2 = 0.01;
     static double GOAL_LUX = 0.053;
     
-    public MyRobot (Vector3d position, String name, Point3d goalPos) {
+    static double SAFETY = 0.8;
+    static double K1 = 5;
+    static double K2 = 0.9;
+    static double K3 = 3;
+    static boolean CLOCKWISE = true;
+    
+    public MyRobot (Vector3d position, String name) {
         super(position, name);
-        status = RobotStatus.REORIENT;
+        status = RobotStatus.MOVE_FWD;
         leftLight = RobotFactory.addLightSensorLeft(this);
         rightLight = RobotFactory.addLightSensorRight(this);
+        sonars = RobotFactory.addSonarBeltSensor(this, 12, 1.5f);
+        status = RobotStatus.MOVE_FWD;
         leftLux = 0;
         rightLux = 0;
         prevLeftLux = 1;
         prevRightLux = 1;
         cnt = 0;
-        goal = goalPos;
-    }
-    
-    @Override
-    public void initBehavior() {
     }
     
     private boolean shouldStop() {
@@ -49,9 +52,6 @@ public class MyRobot extends Agent {
         Point3d pos = new Point3d();
         
         getCoords(pos);
-        
-        System.out.println("dist = " + pos.distance(goal));
-        System.out.println("avgLux = " + avgLux);
         
         return avgLux > GOAL_LUX;
     }
@@ -89,8 +89,55 @@ public class MyRobot extends Agent {
         double avgLux = 0.5*(leftLux + rightLux);
         double goalDiff = GOAL_LUX - avgLux;
         
-        setTranslationalVelocity(K_TRANSL * (Math.log10(Math.abs(goalDiff) + 1) + K_TRANSL_2));
+        setTranslationalVelocity(K_TRANSL*(Math.log10(Math.abs(goalDiff) + 1) + K_TRANSL_2));
         setRotationalVelocity(0);
+    }
+        
+    private double wrapToPi(double a) {
+        while (a > Math.PI) {
+            a -= Math.PI*2;
+        }
+        while (a <= -Math.PI) {
+            a += Math.PI*2;
+        }
+        return a;
+    }
+        
+    private Point3d getSensedPoint(int sonar){        
+        double v = radius + sonars.getMeasurement(sonar);
+        double x = v*Math.cos(sonars.getSensorAngle(sonar));
+        double z = v*Math.sin(sonars.getSensorAngle(sonar));
+        return new Point3d(x, 0, z);
+    }
+    
+    private void circumNavigate() {
+        int min = 0;
+        
+        for (int i = 1; i < sonars.getNumSensors(); i++) {
+            if (sonars.getMeasurement(i) < sonars.getMeasurement(min)) {
+                min = i;
+            }
+        }
+        
+        Point3d p = getSensedPoint(min);
+        double d = p.distance(new Point3d(0, 0, 0));  
+        Vector3d v = CLOCKWISE ? new Vector3d(-p.z, 0, p.x) : new Vector3d(p.z, 0, -p.x);
+        
+        double phLin = Math.atan2(v.z, v.x);
+        double phRot = Math.atan(K3*(d - SAFETY));
+        
+        if (CLOCKWISE) {
+            phRot = -phRot;
+        }
+        
+        double phRef = wrapToPi(phLin + phRot); 
+        
+        setRotationalVelocity(K1*phRef);
+        setTranslationalVelocity(K2*Math.cos(phRef));            
+    }
+    
+    @Override
+    public void initBehavior() {
     }
     
     @Override
